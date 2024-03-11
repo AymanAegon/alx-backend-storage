73fc3d7c8fa5aa233d32365686891c345fd4d5a5
Ayman Ammar <ayman24ammar@gmail.com>
2024-03-11 16:53:23 +0000

    9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""9. Insert a document in Python"""
+
+def insert_school(mongo_collection, **kwargs):
+    """Insert a document in Python"""
+    obj = {}
+    for key, value in kwargs.items():
+        obj[key] = value
+    result = mongo_collection.insert_many(obj)
+    return result
