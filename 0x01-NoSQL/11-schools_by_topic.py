b46ffd837973d747c5a2d0349a18e11979b8593d
Ayman Ammar <ayman24ammar@gmail.com>
2024-03-11 17:34:13 +0000

    11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -5,6 +5,7 @@ def schools_by_topic(mongo_collection, topic):
     """returns the list of school having a specific topic"""
     arr = []
     for school in list(mongo_collection.find()):
+        print(school)
         if topic in school['topics']:
             arr.append(school)
     return arr
