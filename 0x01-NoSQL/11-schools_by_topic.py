00cf8088be59a3180986bef4e4ac0d3a8ecc140a
Ayman Ammar <ayman24ammar@gmail.com>
2024-03-11 17:33:24 +0000

    11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""11. Where can I learn Python?"""
+
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic"""
+    arr = []
+    for school in list(mongo_collection.find()):
+        if topic in school['topics']:
+            arr.append(school)
+    return arr
