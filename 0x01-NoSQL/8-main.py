cf704d89915ad0a0fee607809694b0437b825c32
Ayman Ammar <ayman24ammar@gmail.com>
2024-03-11 16:26:15 +0000

    8

---
+++ 0x01-NoSQL/8-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""List all documents in Python"""
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    """List all documents in Python"""
+    arr = []
+    
